a701da4d9c958cf13360d61004638b832cfa1146
Jaymoh-G <gmorembatia@gmail.com>
2025-08-07 18:31:22 +0300

    seo

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -39,10 +39,22 @@ class GenerateSitemap extends Command
         try {
             $urls = $this->seoService->generateSitemap();
 
-            $content = view('sitemap.index', compact('urls'))->render();
+            $xml = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+            $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">' . "\n";
+
+            foreach ($urls as $url) {
+                $xml .= '    <url>' . "\n";
+                $xml .= '        <loc>' . htmlspecialchars($url['url']) . '</loc>' . "\n";
+                $xml .= '        <lastmod>' . $url['lastmod'] . '</lastmod>' . "\n";
+                $xml .= '        <changefreq>' . $url['changefreq'] . '</changefreq>' . "\n";
+                $xml .= '        <priority>' . $url['priority'] . '</priority>' . "\n";
+                $xml .= '    </url>' . "\n";
+            }
+
+            $xml .= '</urlset>';
 
             // Save to public directory
-            file_put_contents(public_path('sitemap.xml'), $content);
+            file_put_contents(public_path('sitemap.xml'), $xml);
 
             $this->info('Sitemap generated successfully!');
             $this->info('Total URLs: ' . count($urls));
--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -18,9 +18,21 @@ class SitemapController extends Controller
     {
         $urls = $this->seoService->generateSitemap();
 
-        $content = view('sitemap.index', compact('urls'))->render();
+        $xml = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+        $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">' . "\n";
 
-        return response($content, 200)
+        foreach ($urls as $url) {
+            $xml .= '    <url>' . "\n";
+            $xml .= '        <loc>' . htmlspecialchars($url['url']) . '</loc>' . "\n";
+            $xml .= '        <lastmod>' . $url['lastmod'] . '</lastmod>' . "\n";
+            $xml .= '        <changefreq>' . $url['changefreq'] . '</changefreq>' . "\n";
+            $xml .= '        <priority>' . $url['priority'] . '</priority>' . "\n";
+            $xml .= '    </url>' . "\n";
+        }
+
+        $xml .= '</urlset>';
+
+        return response($xml, 200)
             ->header('Content-Type', 'application/xml')
             ->header('Cache-Control', 'public, max-age=3600'); // Cache for 1 hour
     }
@@ -31,10 +43,22 @@ class SitemapController extends Controller
         // to regenerate the sitemap periodically
         $urls = $this->seoService->generateSitemap();
 
-        $content = view('sitemap.index', compact('urls'))->render();
+        $xml = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+        $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">' . "\n";
+
+        foreach ($urls as $url) {
+            $xml .= '    <url>' . "\n";
+            $xml .= '        <loc>' . htmlspecialchars($url['url']) . '</loc>' . "\n";
+            $xml .= '        <lastmod>' . $url['lastmod'] . '</lastmod>' . "\n";
+            $xml .= '        <changefreq>' . $url['changefreq'] . '</changefreq>' . "\n";
+            $xml .= '        <priority>' . $url['priority'] . '</priority>' . "\n";
+            $xml .= '    </url>' . "\n";
+        }
+
+        $xml .= '</urlset>';
 
         // Save to public directory
-        file_put_contents(public_path('sitemap.xml'), $content);
+        file_put_contents(public_path('sitemap.xml'), $xml);
 
         return response()->json(['message' => 'Sitemap generated successfully']);
     }
